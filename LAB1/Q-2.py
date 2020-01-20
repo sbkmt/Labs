5af741d88e1b305bb61e3d24a69733ba0c766967
sbkmt <59813935+sbkmt@users.noreply.github.com>
2020-01-20 08:56:29 +0530

    Add files via upload

---
+++ LAB1/Q-2.py
@@ -0,0 +1,13 @@
+'''Create a program that asks the user to enter
+their name and their age. Print out a message
+addressed to them that tells them the year that
+they will turn 100 years old.'''
+
+print("Enter your name: ")
+x=str(input())
+print("Enter your age")
+y=int(input())
+z=100-y
+w=2020+z
+print("The year in which you will turn 100 is :")
+print(w)
---
+++ LAB1/Q-3.py
@@ -0,0 +1,12 @@
+'''Ask the user for a number. Depending on
+whether the number is even or odd, print out an
+appropriate message to the user.'''
+
+print("Enter the number")
+x=int(input())
+y=x%2
+z=0
+if y==z:
+    print("Number is even")
+else:
+    print("Number is odd")    
